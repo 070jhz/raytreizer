e84d401ff8059a9339c62c37c6eab61be8b0ebc0
jhz <070jhz@proton.me>
2025-06-21 08:32:16 +0200

    refactor rendering logic from main loop to camera module

--- src/camera.rs
+++ src/camera.rs
@@ -1,22 +1,42 @@
+use minifb::{ Key, Window, WindowOptions };
+
+use crate::ray::Ray;
+use crate::scene::Scene;
 use crate::Point3;
 use crate::Vec3;
+use crate::color::Color;
 
 #[derive(Clone)]
 pub struct Viewport {
   pub u       : Vec3,
   pub v       : Vec3,
   pub origin  : Point3, // top-left corner
+  pub pdu                 : Vec3,     // pixel spacing in u direction
+  pub pdv                 : Vec3,     // pixel spacing in v direction
+  pub p00                 : Point3,   // location of top left pixel
+  
 }
 
 #[derive(Clone)]
 pub struct Camera {
+  pub aspect_ratio    : f64,
+  pub image_width     : usize,
   pub position        : Point3,
   pub focal_length    : f64,
   pub viewport        : Viewport,
+  pub image_height        : usize,
 }
 
 impl Camera {
-  pub fn new(position: Point3, fov_degrees: f64, aspect_ratio: f64) -> Self {
+  pub fn new(position: Point3,
+    fov_degrees: f64,
+    aspect_ratio: f64,
+    image_width: usize) -> Self {
+
+    let mut image_height = (image_width as f64 / aspect_ratio) as usize;
+    image_height = image_height.max(1); 
+    let actual_ratio = image_width as f64 / image_height as f64;
+    
     let focal_length = 1.0;
     let viewport_height = 2.0 * focal_length * (fov_degrees.to_radians() / 2.0).tan();
     let viewport_width = viewport_height * aspect_ratio;
@@ -31,16 +51,53 @@ impl Camera {
                           - viewport_u / 2.0
                           - viewport_v / 2.0;
 
+    let pdu         = viewport_u / (image_width as f64);
+    let pdv         = viewport_v / (image_height as f64);
+    let p00: Point3 = viewport_top_left + pdu / 2.0 + pdv / 2.0;
+
     let viewport = Viewport {
       u: viewport_u,
       v: viewport_v,
       origin: viewport_top_left,
+      pdu,
+      pdv,
+      p00,
     };
+      
 
     Camera {
+      aspect_ratio: actual_ratio,
+      image_width,
       position,
       focal_length : 1.0,
       viewport,
+      image_height,
     }
   }
-}
+
+  pub fn render_frame(&self, scene: &Scene) -> Vec<u32> {
+    let mut buffer: Vec<u32> = vec![0; self.image_width * self.image_height];
+    for (i,p) in buffer.iter_mut().enumerate() {
+      let pixel = self.viewport.p00 
+                + ((i % self.image_width) as f64) * self.viewport.pdu
+                + ((i / self.image_width) as f64) * self.viewport.pdv;
+      let ray = Ray::new(self.position, (pixel - self.position).unit());
+
+      *p = Self::ray_color(&ray, &scene).to_rgb_bytes();
+
+    }
+    buffer
+  }
+
+  fn ray_color(ray: &Ray, scene: &Scene) -> Color {
+    if let Some(hit) = scene.hit(ray) {
+      let n = hit.normal;
+      return Color::rgb((n.x + 1.0) / 2.0, (n.y + 1.0) / 2.0, (n.z + 1.0) / 2.0);
+    }
+    let unit_direction = ray.dir.unit();
+    let a = 0.5 * (unit_direction.y + 1.0);
+    let lerp = (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.3, 0.5, 1.0);
+    Color::Rgb(lerp)
+  }
+
+} 
--- src/main.rs
+++ src/main.rs
@@ -10,55 +10,50 @@ use crate::color::Color;
 use crate::math::Point3;
 use crate::ray::Ray;
 use crate::camera::Camera;
-use crate::scene::{ Scene, AmbientLight, object::{ Object, Sphere, Cylinder } };
-
-const ASPECT_RATIO: f64 = 16.0 / 9.0;
-const WIDTH: usize = 1280;
-
-fn ray_color(ray: &Ray, scene: &Scene) -> Color {
-  if let Some(hit) = scene.hit(ray) {
-    let n = hit.normal;
-    return Color::rgb((n.x + 1.0) / 2.0, (n.y + 1.0) / 2.0, (n.z + 1.0) / 2.0);
-  }
-  let unit_direction = ray.dir.unit();
-  let a = 0.5 * (unit_direction.y + 1.0);
-  let lerp = (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(1.0, 0.5, 0.7);
-  Color::Rgb(lerp)
-}
+use crate::scene::{ Scene, AmbientLight, object::{ Object, Sphere, Cylinder, Plane } };
 
 fn main() {
-  let mut height = (WIDTH as f64 / ASPECT_RATIO) as usize;
-  height = if height < 1 { 1 } else { height };
-  let actual_ratio = WIDTH as f64 / height as f64;
-  
   let mut scene = Scene::new(
     Camera::new(
       Point3::new(0.0, 0.0, 0.0), 
       45.0, 
-      actual_ratio,
+      16.0 / 9.0,
+      1280,
     ),
     AmbientLight { 
       ratio: 0.2,
       color: Color::rgb(0.8, 0.8, 0.8),
     },
-  );
+  );  
 
-  let camera = scene.camera.clone();
-  let viewport = &camera.viewport;
+  let sp = Sphere {
+    center: Point3::new(0.25, 0.0, -1.0),
+    radius: 0.125,
+  };
 
-  // pixel delta vectors
-  let pdu = viewport.u / (WIDTH as f64);
-  let pdv = viewport.v / (height as f64);
-  
-  // starting pixel (0,0)
-  let p00 = viewport.origin + pdu / 2.0 + pdv / 2.0;
+  let cyl = Cylinder {
+    center: Point3::new(0.0, 0.0, -5.0),
+    radius: 0.125,
+    height: 0.5,
+    orientation: Vec3::new(0.2, 0.0, -1.0).unit(),
+  };
+
+  let pl = Plane {
+    anchor: Point3::new(0.0, -0.0, -1.0),
+    normal: Vec3::new(0.0, 1.0, 0.0).unit(),
+  };
+
+  scene.add_object(Object::Sphere(sp));
+  scene.add_object(Object::Cylinder(cyl));
+  scene.add_object(Object::Plane(pl));
+
+  let camera = &scene.camera;
   
   // window setup
-  let mut buffer: Vec<u32> = vec![0; WIDTH * height];
   let mut window = Window::new(
     "Test",
-    WIDTH,
-    height, 
+    camera.image_width,
+    camera.image_height, 
     WindowOptions {
       resize: true,
       ..WindowOptions::default()
@@ -68,29 +63,10 @@ fn main() {
   });
   window.set_target_fps(60);
   
-  let sp = Sphere {
-    center: Point3::new(0.25, 0.0, -1.0),
-    radius: 0.125,
-  };
-
-  let cyl = Cylinder {
-    center: Point3::new(0.0, 0.0, -5.0),
-    radius: 0.25,
-    height: 1.0,
-    orientation: Vec3::new(0.0, -0.5, 1.0).unit(),
-  };
-
-  scene.add_object(Object::Sphere(sp));
-  scene.add_object(Object::Cylinder(cyl));
-
+ 
   while window.is_open() && !window.is_key_down(Key::Escape) {
-    for (i,p) in buffer.iter_mut().enumerate() {
-      let pixel = p00 + ((i % WIDTH) as f64) * pdu + ((i / WIDTH) as f64) * pdv;
-      let ray = Ray::new(camera.position, (pixel - camera.position).unit());
-
-      *p = ray_color(&ray, &scene).to_rgb_bytes();
-    }
-    window.update_with_buffer(&buffer, WIDTH, height).unwrap();
+    let buffer = camera.render_frame(&scene);
+    window.update_with_buffer(&buffer, camera.image_width, camera.image_height).unwrap();
   }
 }
 
