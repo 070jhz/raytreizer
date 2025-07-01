497dcc3f0bd8745795b4fd997a88a29bf789bd6d
jhz <070jhz@proton.me>
2025-07-01 20:42:13 +0200

    implement anti aliasing - 4x stochastic SSAA - use custom struct FrameBuffer - refactor ray_color to return Vec3 instead of Color

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -1,3 +1,5 @@
+use crate::framebuffer::FrameBuffer;
+use crate::utils::random_double_in;
 use crate::Color;
 use crate::Camera;
 use crate::Ray;
@@ -40,21 +42,28 @@ impl Scene {
     //self.lights.clear();
   }
 
-  pub fn render_frame(&self) -> Vec<u32> {
-    let mut buffer: Vec<u32> = vec![0; self.camera.image_width * self.camera.image_height];
-    for (i,p) in buffer.iter_mut().enumerate() {
-      let pixel = self.camera.viewport.p00 
-                + ((i % self.camera.image_width) as f64) * self.camera.viewport.pdu
-                + ((i / self.camera.image_width) as f64) * self.camera.viewport.pdv;
-      let ray = Ray::new(self.camera.position, (pixel - self.camera.position).unit());
+  pub fn render_frame(&self) -> FrameBuffer {
+    let mut buffer = FrameBuffer::new(self.camera.image_width, self.camera.image_height);
+    for j in 0..self.camera.image_height {
+      for i in 0..self.camera.image_width {
+        // anti aliasing
+        let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
 
-      *p = Self::ray_color(&self, &ray).to_rgb_bytes();
+        for _sample in 0..self.camera.sampling_rate {
+          let pixel = self.camera.viewport.p00
+                    + (i as f64 + random_double_in(-0.5, 0.5)) * self.camera.viewport.pdu
+                    + (j as f64 + random_double_in(-0.5, 0.5)) * self.camera.viewport.pdv;
+          let ray = Ray::new(self.camera.position, (pixel - self.camera.position).unit());
+          pixel_color += self.ray_color(&ray);
+        }
 
+        buffer[(i, j)] = Color::Rgb(pixel_color / (self.camera.sampling_rate as f64)).to_rgb_bytes();
+      }
     }
     buffer
   }
   
-  pub fn hit(&self,ray: &Ray) -> Option<HitRecord> {
+  fn cast(&self,ray: &Ray) -> Option<HitRecord> {
     let mut closest   : Option<HitRecord> = None;
     let mut closest_t = f64::INFINITY;
     
@@ -70,15 +79,14 @@ impl Scene {
     closest
   }
 
-  fn ray_color(&self, ray: &Ray) -> Color {
-    if let Some(hit) = self.hit(ray) {
+  fn ray_color(&self, ray: &Ray) -> Vec3 {
+    if let Some(hit) = self.cast(ray) {
       let n = hit.normal;
-      return Color::rgb((n.x + 1.0) / 2.0, (n.y + 1.0) / 2.0, (n.z + 1.0) / 2.0);
+      return Vec3::new((n.x + 1.0) / 2.0, (n.y + 1.0) / 2.0, (n.z + 1.0) / 2.0);
     }
     let unit_direction = ray.dir.unit();
     let a = 0.5 * (unit_direction.y + 1.0);
-    let lerp = (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.3, 0.5, 1.0);
-    Color::Rgb(lerp)
+    (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.3, 0.5, 1.0)
   }
 
 }
