2da19ca4e1aadad2e005334f42b1a1547be7f949
jhz <070jhz@proton.me>
2025-07-01 20:42:55 +0200

    add antialiasing settings

--- src/camera.rs
+++ src/camera.rs
@@ -1,10 +1,5 @@
-use minifb::{ Key, Window, WindowOptions };
-
-use crate::ray::Ray;
-use crate::scene::Scene;
 use crate::Point3;
 use crate::Vec3;
-use crate::color::Color;
 
 #[derive(Clone)]
 pub struct Viewport {
@@ -24,7 +19,8 @@ pub struct Camera {
   pub position        : Point3,
   pub focal_length    : f64,
   pub viewport        : Viewport,
-  pub image_height        : usize,
+  pub image_height    : usize,
+  pub sampling_rate   : usize,
 }
 
 impl Camera {
@@ -66,38 +62,14 @@ impl Camera {
       
 
     Camera {
-      aspect_ratio: actual_ratio,
+      aspect_ratio  : actual_ratio,
       image_width,
       position,
-      focal_length : 1.0,
+      focal_length  : 1.0,
       viewport,
       image_height,
+      sampling_rate : 4, 
     }
   }
 
-  pub fn render_frame(&self, scene: &Scene) -> Vec<u32> {
-    let mut buffer: Vec<u32> = vec![0; self.image_width * self.image_height];
-    for (i,p) in buffer.iter_mut().enumerate() {
-      let pixel = self.viewport.p00 
-                + ((i % self.image_width) as f64) * self.viewport.pdu
-                + ((i / self.image_width) as f64) * self.viewport.pdv;
-      let ray = Ray::new(self.position, (pixel - self.position).unit());
-
-      *p = Self::ray_color(&ray, &scene).to_rgb_bytes();
-
-    }
-    buffer
-  }
-
-  fn ray_color(ray: &Ray, scene: &Scene) -> Color {
-    if let Some(hit) = scene.hit(ray) {
-      let n = hit.normal;
-      return Color::rgb((n.x + 1.0) / 2.0, (n.y + 1.0) / 2.0, (n.z + 1.0) / 2.0);
-    }
-    let unit_direction = ray.dir.unit();
-    let a = 0.5 * (unit_direction.y + 1.0);
-    let lerp = (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.3, 0.5, 1.0);
-    Color::Rgb(lerp)
-  }
-
-} 
+  } 
