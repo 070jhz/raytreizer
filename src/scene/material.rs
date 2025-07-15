604729a70a8f7f0e5330f01809d152fc6700485a
jhz <070jhz@proton.me>
2025-07-15 04:15:50 +0200

    implement basic metal material with fuzzing

--- src/scene/material.rs
+++ src/scene/material.rs
@@ -1,4 +1,4 @@
-use crate::{color::Color, math::Vec3, ray::Ray};
+use crate::{color::Color, math::{Vec3, EPSILON}, ray::Ray};
 use crate::scene::HitRecord;
 
 pub trait Material {
@@ -10,10 +10,16 @@ pub struct Solid {
   pub albedo: Color,
 }
 
+pub struct BasicMetal {
+  pub albedo: Color,
+  pub fuzz: f64,
+}
+
 impl Material for Solid {
   fn albedo(&self) -> Color {
       self.albedo
   }
+
   fn scatter(&self, _ray: &Ray, rec: &HitRecord) -> Option<(Ray, Color)> {
     let mut scatter_dir = rec.normal + Vec3::random_unit_sphere();
     
@@ -22,7 +28,34 @@ impl Material for Solid {
     }
     
     let offset_origin = rec.point + rec.normal * 1e-4;
-    Some( (Ray::new(offset_origin, scatter_dir), self.albedo()) )
+    Some( (Ray::new(offset_origin, scatter_dir.unit()), self.albedo()) )
     
+  }
+}
+
+impl Material for BasicMetal {
+  fn albedo(&self) -> Color {
+      self.albedo
+  }
+
+  fn scatter(&self, ray: &Ray, rec: &HitRecord) -> Option<(Ray, Color)> {
+    let reflected_dir = ray.dir - 2.0 * ray.dir.dot(&rec.normal) * rec.normal;    
+    
+    if reflected_dir.dot(&rec.normal) < EPSILON {
+      return None;
+    }
+
+    let mut random_offset = Vec3::random_unit_sphere();
+    if random_offset.dot(&rec.normal) < 0.0 {
+      random_offset= -random_offset;
+    }
+
+    let fuzzed = reflected_dir + (self.fuzz * random_offset);
+    if fuzzed.length_squared() < EPSILON * EPSILON {
+      return None;
+    }
+
+    let offset_origin = rec.point + rec.normal * EPSILON;
+    Some( (Ray::new(offset_origin, fuzzed.unit()), self.albedo()) )
   }
 }
